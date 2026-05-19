80916c6fa07f60e6cf17f30667747ebbec2fb083
Parth Mudgal <artpar@gmail.com>
2026-05-19 14:36:46 +0530

    Add playwright.config.ts

---
+++ playwright.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: false,
+  forbidOnly: !!process.env.CI,
+  retries: 2,
+  workers: 1,
+  reporter: 'list',
+  use: {
+    baseURL: 'https://www.myscheme.gov.in',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+  timeout: 60000,
+});
